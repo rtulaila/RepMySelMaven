7f78b107c791312bbbdff23015885c81170b13a5
rtulaila <rameshkqa@gmail.com>
2020-12-31 13:33:15 -0500

    Fixed TestNG test to compile

--- MySelMaven/src/main/java/Selpackage/Selclass.java
+++ MySelMaven/src/main/java/Selpackage/Selclass.java
@@ -2,18 +2,32 @@ package Selpackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Selclass {
 
-	public static void main(String[] args) {
+	@Test
+	public  void verifyTest()
+	{
 		// TODO Auto-generated method stub
    
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramesh\\Downloads\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe") ;
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.ca/");
-		System.out.println(driver.getTitle());
 		
+		driver.get("https://www.google.ca/");
+		
+		System.out.println("===============================");
+		System.out.println("                               ");  
+		System.out.println("Chrome Driver Connected");
+		System.out.println("Opened GOOGLE");
+		System.out.println("Getting Title");
+		System.out.println(driver.getTitle());
+		System.out.println("Got Title");
+		System.out.println(driver.getTitle());
+		System.out.println("                               ");  
+		System.out.println("===============================");
+				
 		driver.quit();
 	}
 }
